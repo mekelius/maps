3058e59a78d70503a8a96775cfa18ee9117577a8
Mihkal Jouste <mihkal.jouste@gmail.com>
2025-04-06 16:04:05 +0300

    move sources to src

--- src/verify_main.cpp
+++ src/verify_main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <memory>
 
 #include "parser.hh"
 #include "lexer.hh"
 
 int main(int argc, char* argv[]) {
+    std::unique_ptr<StreamingLexer> lexer = std::make_unique<StreamingLexer>(&std::cin, &std::cerr);
+    
     return EXIT_SUCCESS;
 }
