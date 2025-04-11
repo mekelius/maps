3331afea4bd586ba3d716c0c19cabd355b279915
Mihkal Jouste <mihkal.jouste@gmail.com>
2025-04-11 22:51:56 +0300

    first attempt at some kind of complex type

--- src/lang/types.hh
+++ src/lang/types.hh
@@ -1,6 +1,9 @@
 #ifndef __TYPES_HH
 #define __TYPES_HH
 
+#include <optional>
+#include <variant>
+
 namespace AST {
 
 // class for booleans we may or may not know yet
@@ -10,42 +13,68 @@ enum class DeferredBool {
     unknown,
 };
 
+struct FunctionType;
+using ComplexType = std::variant<std::monostate, FunctionType>;
+
 struct Type {
     bool is_native = false;
     std::string_view name;
     DeferredBool is_numeric = DeferredBool::false_;
     DeferredBool is_integral = DeferredBool::false_;
+
+    // this seems like a sane way to implement complex types without inheritance or 
+    // whole-type variant
+    ComplexType ct = std::monostate{};
+
+    bool is_complex() const {
+        return !std::holds_alternative<std::monostate>(ct);
+    }
+    
+    unsigned int arity() const {
+        if (const auto function_type = std::get_if<FunctionType>(&ct))
+            return function_type->arity();
+        return 0;
+    }
 };
 
-constexpr Type Int {
+struct FunctionType {
+    Type* return_type;
+    std::vector<Type*> parameter_types = {};
+
+    unsigned int arity() const {
+        return parameter_types.size();
+    }
+};
+
+const Type Int {
     true,
     "Int",
     DeferredBool::true_,
     DeferredBool::true_,
 };
 
-constexpr Type Boolean {
+const Type Boolean {
     true,
     "Boolean",
     DeferredBool::false_,
     DeferredBool::false_,
 };
 
-constexpr Type String {
+const Type String {
     true,
     "String",
     DeferredBool::false_,
     DeferredBool::false_,
 };
 
-constexpr Type Void {
+const Type Void {
     true,
     "Void",
     DeferredBool::false_,
     DeferredBool::false_,
 };
 
-constexpr Type Hole {
+const Type Hole {
     false,
     "Hole",
     DeferredBool::unknown,
@@ -53,7 +82,7 @@ constexpr Type Hole {
 };
 
 // a number who's type hasn't yet been determined
-constexpr Type NumberLiteral {
+const Type NumberLiteral {
     false,
     "NumberLiteral",
     DeferredBool::true_,
