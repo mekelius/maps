5b0a51eb48d2a22163a60655c5df0aa7d64af749
Mihkal Jouste <mihkal.jouste@gmail.com>
2025-05-03 03:03:47 +0300

    skeleton

--- src/mapsc/types/type_constructor.cpp
+++ src/mapsc/types/type_constructor.cpp
@@ -6,28 +6,31 @@
 
 namespace Maps {
 
-// TypeConstructor::TypeConstructor(const std::string& name, int arity)
-// :name_(name), arity_(arity) {}
+TypeConstructor::TypeConstructor(const std::string& name, int arity)
+:name_(name), arity_(arity) {}
 
-// ImpureFunctionConstructor::ImpureFunctionConstructor()
-// :TypeConstructor("=>", TypeConstructor::ARITY_N) {}
+ImpureFunctionConstructor::ImpureFunctionConstructor()
+:TypeConstructor("=>", TypeConstructor::ARITY_N) {}
 
-// const Type* ImpureFunctionConstructor::make_type(TypeRegistry& type_registry, 
-//     std::vector<TypeConstructor::TypeArg>&& args, std::string* name) {
-//     assert(false && "not implemented");    
-// }
+const Type* ImpureFunctionConstructor::make_type(TypeRegistry& type_registry, 
+    std::vector<TypeConstructor::TypeArg>&& args, std::string* name) {
+    assert(false && "not implemented");    
+}
 
-// PureFunctionConstructor::PureFunctionConstructor()
-// :TypeConstructor("->", TypeConstructor::ARITY_N) {}
+PureFunctionConstructor::PureFunctionConstructor()
+:TypeConstructor("->", TypeConstructor::ARITY_N) {}
 
-// const Type* PureFunctionConstructor::make_type(TypeRegistry& type_registry, 
-//     std::vector<TypeConstructor::TypeArg>&& args, std::string* name) {
+const Type* PureFunctionConstructor::make_type(TypeRegistry& type_registry, 
+    std::vector<TypeConstructor::TypeArg>&& args_, std::string* name) {
 
-//     assert(args.size() > 0 && "PureFuntionConstructor::make_type called with no args");
+    auto args = args_;
 
-//     Type* return_type = args.back();
-//     type_registry.get_function_type(args.at(0), );
-//     return nullptr;
-// }
+    assert(args.size() > 0 && "PureFuntionConstructor::make_type called with no args");
+
+    Type* return_type = args.back();
+    args.pop_back();
+    type_registry.get_function_type(return_type, args.at(0), true);
+    return nullptr;
+}
 
 } // namespace Maps
--- src/mapsc/types/type_constructor.hh
+++ src/mapsc/types/type_constructor.hh
@@ -9,53 +9,53 @@
 
 namespace Maps {
 
-// class Type;
-// class TypeRegistry;
+class Type;
+class TypeRegistry;
 
-// class TypeConstructor {
-// public:
-//     using TypeArg = std::tuple<const Type*, std::optional<std::string>>;
+class TypeConstructor {
+public:
+    using TypeArg = std::tuple<const Type*, std::optional<std::string>>;
 
-//     TypeConstructor(const std::string& name, int arity);
+    TypeConstructor(const std::string& name, int arity);
 
-//     virtual ~TypeConstructor() = default;
-//     TypeConstructor(TypeConstructor&) = delete;
-//     TypeConstructor& operator=(TypeConstructor&) = delete;
+    virtual ~TypeConstructor() = default;
+    TypeConstructor(TypeConstructor&) = delete;
+    TypeConstructor& operator=(TypeConstructor&) = delete;
 
-//     virtual const Type* make_type(TypeRegistry& type_registry, std::vector<TypeArg>&& args, 
-//         std::string* name = nullptr) = 0;
+    virtual const Type* make_type(TypeRegistry& type_registry, std::vector<TypeArg>&& args, 
+        std::string* name = nullptr) = 0;
     
-//     static constexpr int ARITY_N = -1;
+    static constexpr int ARITY_N = -1;
     
-//     std::string name_;
-//     const int arity_ = 1;
-// };
+    std::string name_;
+    const int arity_ = 1;
+};
     
-// class PureFunctionConstructor: public TypeConstructor {
-// public:
-//     PureFunctionConstructor();
-//     const Type* make_type(TypeRegistry& type_registry, std::vector<TypeArg>&& args, 
-//         std::string* name = nullptr);
-// };
-// static const PureFunctionConstructor PureFunction{};
+class PureFunctionConstructor: public TypeConstructor {
+public:
+    PureFunctionConstructor();
+    const Type* make_type(TypeRegistry& type_registry, std::vector<TypeArg>&& args, 
+        std::string* name = nullptr);
+};
+static const PureFunctionConstructor PureFunction{};
 
-// class ImpureFunctionConstructor: public TypeConstructor {
-// public:
-//     ImpureFunctionConstructor();
-//     const Type* make_type(TypeRegistry& type_registry, std::vector<TypeArg>&& args, 
-//         std::string* name = nullptr);
-// };
-// static const PureFunctionConstructor ImpureFunction{};
+class ImpureFunctionConstructor: public TypeConstructor {
+public:
+    ImpureFunctionConstructor();
+    const Type* make_type(TypeRegistry& type_registry, std::vector<TypeArg>&& args, 
+        std::string* name = nullptr);
+};
+static const PureFunctionConstructor ImpureFunction{};
 
-// // static constexpr TypeConstructor optional;
-// // static constexpr TypeConstructor tuple;
-// // static constexpr TypeConstructor union_tc;
-// // static constexpr TypeConstructor sequence;
+// static constexpr TypeConstructor optional;
+// static constexpr TypeConstructor tuple;
+// static constexpr TypeConstructor union_tc;
+// static constexpr TypeConstructor sequence;
 
-// static const std::array<const TypeConstructor*, 2> BUILTIN_TYPECONSTRUCTORS = {
-//     &PureFunction, 
-//     &ImpureFunction, 
-// };
+static const std::array<const TypeConstructor*, 2> BUILTIN_TYPECONSTRUCTORS = {
+    &PureFunction, 
+    &ImpureFunction, 
+};
 
 } // namespace Maps
 
