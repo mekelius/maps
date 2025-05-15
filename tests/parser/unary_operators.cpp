4a065fa309a8729db3ba7cb6e2f761ad2ea6da52
Mihkal Jouste <mihkal.jouste@gmail.com>
2025-05-16 01:04:00 +0300

    add some simple unary tests

--- tests/parser/unary_operators.cpp
+++ tests/parser/unary_operators.cpp
@@ -53,3 +53,73 @@ TEST_CASE("postfix purely unary operator") {
     CHECK(**args.begin() == value);
     CHECK(callee == &op);
 }
+
+TEST_CASE("Unary should take precedence over binary (prefix)") {
+    auto [state, _0, types] = CompilationState::create_test_state();
+
+    REQUIRE(state.is_valid);
+
+    auto op = Operator{"!", External{}, *types->get_function_type(Boolean, {&Boolean}, true), 
+        {UnaryFixity::prefix, BinaryFixity::infix}, TSL};
+    auto op_ref = Expression{ExpressionType::operator_reference, TSL, &op, op.get_type()};
+    auto value = Expression{ExpressionType::value, TSL, true, &Boolean};
+
+    SUBCASE("!true") {
+        auto expr = Expression{ExpressionType::termed_expression, TSL, 
+            TermedExpressionValue{{&op_ref, &value}}};
+    
+        TermedExpressionParser{&state, &expr}.run();
+    
+        CHECK(state.is_valid);
+    
+        CHECK(expr.expression_type == ExpressionType::call);
+        
+        auto [callee, args] = expr.call_value();
+    
+        CHECK(args.size() == 1);
+        CHECK(**args.begin() == value);
+        CHECK(callee == &op);
+    }
+
+    // SUBCASE("true ! false") {
+    //     auto value = Expression{ExpressionType::value, TSL, true, &Boolean};
+
+    //     auto expr = Expression{ExpressionType::termed_expression, TSL, 
+    //         TermedExpressionValue{{&op_ref, &value}}};
+    
+    //     TermedExpressionParser{&state, &expr}.run();
+    
+    //     CHECK(state.is_valid);
+    
+    //     CHECK(expr.expression_type == ExpressionType::call);
+        
+    //     auto [callee, args] = expr.call_value();
+    
+    //     CHECK(args.size() == 1);
+    //     CHECK(**args.begin() == value);
+    //     CHECK(callee == &op);
+    // }
+}
+
+TEST_CASE("Unary should take precedence over binary (postfix)") {
+    auto [state, _0, types] = CompilationState::create_test_state();
+
+    auto op = Operator{"!", External{}, *types->get_function_type(Boolean, {&Boolean}, true), 
+        {UnaryFixity::postfix, BinaryFixity::infix}, TSL};
+    auto op_ref = Expression{ExpressionType::operator_reference, TSL, &op, op.get_type()};
+    auto value = Expression{ExpressionType::value, TSL, true, &Boolean};
+    auto expr = Expression{ExpressionType::termed_expression, TSL, 
+        TermedExpressionValue{{&value, &op_ref}}};
+
+    TermedExpressionParser{&state, &expr}.run();
+
+    CHECK(state.is_valid);
+
+    CHECK(expr.expression_type == ExpressionType::call);
+    
+    auto [callee, args] = expr.call_value();
+
+    CHECK(args.size() == 1);
+    CHECK(**args.begin() == value);
+    CHECK(callee == &op);
+}
