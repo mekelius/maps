6a5c5fcfbdd27976193149fdcf7f1362d933e576
Mihkal Jouste <mihkal.jouste@gmail.com>
2025-05-24 16:58:53 +0300

    add some sanity checks for type comparison

---
+++ tests/types/types.cpp
@@ -0,0 +1,40 @@
+#include "doctest.h"
+
+#include "mapsc/types/type.hh"
+#include "mapsc/types/type_store.hh"
+#include "mapsc/types/type_defs.hh"
+#include "mapsc/ast/expression.hh"
+#include "mapsc/ast/ast_store.hh"
+
+using namespace Maps;
+
+TEST_CASE("Should be able to compare types") {
+    TypeStore types{};
+
+    SUBCASE("Simply") {
+        CHECK(Float == Float);
+        CHECK(Int != Float);
+    }
+
+    SUBCASE("From store to global") {
+        CHECK(Float == **types.get("Float"));
+        CHECK(**types.get("Float") == Float);
+
+        CHECK(Int != **types.get("Float"));
+        CHECK(**types.get("Int") != Float);
+    }
+}
+
+TEST_CASE("Should be able to compare types of expressions") {
+    TypeStore types{};
+    AST_Store store{};
+
+    SUBCASE("Expression to global") {
+        auto int_e = Expression::known_value(store, 123, TSL);
+        auto string_e = Expression::known_value(store, "qweqe", TSL);
+
+        CHECK(*int_e->type == Int);
+        CHECK(*string_e->type == String);
+    }
+}
+
